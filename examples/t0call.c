5cc12939b610da03021c6dc6c084a65b92f9e019
Charles Blake <charlechaud@gmail.com>
2020-06-21 19:45:32 -0400

    Make test program smarter/more useful.

--- examples/t0call.c
+++ examples/t0call.c
@@ -1,9 +1,12 @@
 #include <stdio.h>		// printf
+#include <errno.h>		// errno
+#include <string.h>		// strerror
 #include "linux/batch.h"	// syscall_t batch
 
 int main(int ac, char **av) {
 	syscall_t bat = { 0 };
-	long	  rvs = 0;
-	printf("%ld\n", batch(&rvs, &bat, 0, 2, 3));
+	long      rvs = 0;
+	long      ret = batch(&rvs, &bat, 0, 2, 3);
+	printf("%ld: %s\n", ret, strerror(errno));
 	return 0;
 }
