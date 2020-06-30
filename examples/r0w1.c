c6f34648eb171c4f32b79c83823df487b128206f
Charles Blake <charlechaud@gmail.com>
2020-06-30 14:42:01 -0400

    Fix to not use clunky signals but just a new max bytes limit cmd param.
    This leads to some interesting results (that arguably may mostly only
    mean people should setvbuf their stdio buffers up to 128K, but could be
    also interpreted as sys_batch helps use a ton of precious L2 cache on
    stuff besides IO buffers):
    
    Times from an i7-6700k at 4.7GHz.  Below should work in bash after
    having done
      z=/dev/zero; n=/dev/null
    
    Monster 3.35x speed-up at 4 KiB buffers
      for try in {1..5}; do BATCH_EMUL=1 ./r0w1 $[1<<12] $[1<<7] $[4<<30] <$z >$n; done
        ~4.3 GB/s
      for try in {1..5}; do ./r0w1 $[1<<12] $[1<<7] $[4<<30] <$z >$n; done
        ~14.4 GB/s
    
    Still great gain of 1.9x at 16 KiB buffers:
      for try in {1..5}; do ./r0w1 $[1<<14] $[1<<7] $[4<<30] <$z >$n; done
        ~21.7 GB/s
      for try in {1..5}; do BATCH_EMUL=1 ./r0w1 $[1<<14] $[1<<7] $[4<<30] <$z >$n; done
        ~11.6 GB/s
    
    Still noteworthy gain of 1.26x speed-up at 64 KiB buffers
      for try in {1..5}; do BATCH_EMUL=1 ./r0w1 $[1<<16] $[1<<7] $[4<<30] <$z >$n; done
        ~19.5 GB/s
      for try in {1..5}; do ./r0w1 $[1<<16] $[1<<7] $[4<<30] <$z >$n; done
        ~24.6 GB/s
    
    Modest/negligible 1.03x speed-up at 512 KiB buffers
      for try in {1..5}; do BATCH_EMUL=1 ./r0w1 $[1<<19] $[1<<7] $[4<<30] <$z >$n; done
        ~23.5 GB/s
      for try in {1..5}; do ./r0w1 $[1<<19] $[1<<7] $[4<<30] <$z >$n; done
        ~24.2 GB/s

--- examples/r0w1.c
+++ examples/r0w1.c
@@ -3,19 +3,11 @@
 #include <stdlib.h>      // malloc, free
 #include <string.h>      // strerror()
 #include <time.h>        // struct timespec, clock_* CLOCK_*
-#include <signal.h>      // signal SIGINT
 #include "linux/batch.h" // syscall_t, batch*|BATCH* macros
-
-long count;
-void sig(int signum) {
-	fprintf(stderr, "%ld buffers copiedn\n", count);
-	exit(0);
-}
 
 // Populate `c` with a `3*n` syscall program referring to `r` that does read-
 // write cycles until either `read()<=0`, `write()<=0`, or batch is exhausted.
 // Using the Linux splice system instead should be quite a bit faster.
-
 void mkRW(syscall_t *c, long *r, int n, int fI, int fO, char *buf, size_t len) {
 	for (int i = 0; i < n; i++) {
 		c[3*i+0] = scall3(read,  -1, -1, 0, fI, buf, len);
@@ -27,28 +19,28 @@ void mkRW(syscall_t *c, long *r, int n, int fI, int fO, char *buf, size_t len) {
 int main(int argc, char *argv[]) {
 	struct timespec t0, t1;
 	if (argc < 2) {
-		fprintf(stderr, "Usage:\n\t[BATCH_EMUL=1] %s BUF_LEN [NBATCH (128)]\n"
-		        "copies stdin to stdout via sys_batch or not\n",
-		        argv[0]);
+		fprintf(stderr, "Usage:\n\t"
+		        "[BATCH_EMUL=1] %s BUF_LEN [[NBATCH (128)] MAX_BYTES (0)]\n"
+		        "copies stdin to stdout via sys_batch or not\n", argv[0]);
 		return 1;
 	}
 	int        len   = atoi(argv[1]);
 	int        n     = argc > 2 ? atoi(argv[2]) : 128;
+	long       bytes = argc > 2 ? atol(argv[3]) : 0;
+	long       copied = 0;
 	char      *buf   = malloc(len);   // Allocate IO, Rets, Calls Buffers
 	long      *rets  = (long *)malloc(3*n * sizeof *rets);
 	syscall_t *calls = (syscall_t *)malloc(3*n * sizeof *calls);
 	if (!buf || !rets || !calls)
 		return 1;
-	signal(SIGINT, sig);            // Install sig() for SIGINT
 	mkRW(calls, rets, n, 0, 1, buf, len);
 	clock_gettime(CLOCK_MONOTONIC, &t0);
-	while (batch(rets, calls, 3*n, 0, 0) == 3*n - 1) {
-		count++;
-//XXX Analyze rets for short writes/errs; Maybe print stuff, etc.
-//		for (int i=0; i < 3*n; i++) fprintf(stderr, "ret%d: %ld\n", i, rets[i]);
-        }
+	while (batch(rets, calls, 3*n, 0, 0) == 3*n - 1)
+		if (bytes > 0 && (copied += n * len) >= bytes)
+			break; //XXX Analyze 3*n rets for short writes/errs/print stuff..
 	clock_gettime(CLOCK_MONOTONIC, &t1);
-	fprintf(stderr, "%ld ns\n",
-	        (t1.tv_sec - t0.tv_sec) * 1000000000 + t1.tv_nsec - t0.tv_nsec);
+	long dt = (t1.tv_sec - t0.tv_sec)*1000000000 + t1.tv_nsec - t0.tv_nsec;
+	fprintf(stderr, "%ld ns  %ld evenBytes  %.3f GB/s\n",
+	        dt, copied, copied/(double)dt);
 	return 0;
 }
