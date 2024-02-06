246f0e35fab5fd295b27174c83fc8ef90d47a7b6
Charles Blake <charlechaud@gmail.com>
2024-02-06 08:57:36 -0500

    Make these test programs more clear.

--- examples/tbad1.c
+++ examples/tbad1.c
@@ -3,8 +3,6 @@
 
 int main(int ac, char **av) {
 	syscall_t bat = { __NR_getuid };
-	long	  rv  = 0;
 	printf("%ld\n", batch(NULL, &bat, 1, 0, 0));
-	printf("%ld\n", rv);
-	return 0;
+	return 0;          // NULL ret ^ should induce seg fault
 }
--- examples/tbad2.c
+++ examples/tbad2.c
@@ -3,8 +3,8 @@
 
 int main(int ac, char **av) {
 	syscall_t bat __attribute((unused)) = { __NR_getuid };
-	long	  rv  = 0;
+	long	  rv  = 0; // pass |||| bad address
 	printf("%ld\n", batch(&rv, NULL, 1, 0, 0));
-	printf("%ld\n", rv);
+	printf("%ld %d\n", rv, errno);  // errno -l|grep this should == EFAULT
 	return 0;
 }
