f8617f65fcb2c115b5ceeac9f602b99c7f76dbb0
Charles Blake <charlechaud@gmail.com>
2025-08-24 02:18:54 -0400

    Try to comment total.c better.  Explain the "high"-level program that was flattened/compiled and label the trivalency column and space out the "column indices" to perhaps give a more clear visual indication of their role as column labels corresponding to leading /*ROW*/scall* row labels.

--- examples/total.c
+++ examples/total.c
@@ -6,11 +6,17 @@
 #include <sys/stat.h>       /* struct stat */
 #include "linux/batch.h"    /* syscall_t, scall<N> macros, batch */
 
+/* Below, `b` is a "manually compiled/flattened" version of the 4 calls:
+  if ((fd = open(path, O_RDONLY) >= 0)) {   // -1,0,0 => continue only if >= 0
+    fstat(fd, &st);                         // error ignored; Could check later
+    void *rval = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    close(fd);                              // error ignored; Could check later
+  } // return value of batch() indicates amount of progress made. */
 char *mapall(char *path, long *len, int *eno) { /* could take prot/flags/etc. */
 	struct stat st;
 	/* Might also init ret[] to impossible -4095 (greatest abs errno) */
 	long        ret[10]={0}, fdAdr=(long)&ret[0], szAdr=(long)&st.st_size,r;
-	syscall_t   b[10] = {   /* 0  1  2  3  4  5 .arg[]s */
+	syscall_t b[10]={/* < = >  0            1  2  3  4  5 .arg[]s */
 	/*0*/scall3(open , -1,0,0, path, O_RDONLY, 0),   // get fd
 	/*1*/scall2(wdcpy, -1,0,0, &b[4].arg[0], fdAdr), // ->fstat
 	/*2*/scall2(wdcpy, -1,0,0, &b[8].arg[0], fdAdr), // ->close post mapFail
